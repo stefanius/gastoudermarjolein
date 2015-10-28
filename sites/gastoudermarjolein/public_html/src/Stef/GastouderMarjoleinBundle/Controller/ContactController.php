41d6390cc2534d0d4b06ee4b5e6bd5c26e611d5e
stefanius <KIT_kat_16>
2015-10-28 19:31:21 +0100

    Implement contact form.

--- sites/gastoudermarjolein/public_html/src/Stef/GastouderMarjoleinBundle/Controller/ContactController.php
+++ sites/gastoudermarjolein/public_html/src/Stef/GastouderMarjoleinBundle/Controller/ContactController.php
@@ -2,12 +2,45 @@
 
 namespace Stef\GastouderMarjoleinBundle\Controller;
 
+use Stef\GastouderMarjoleinBundle\Entity\Contact;
+use Stef\GastouderMarjoleinBundle\Form\ContactType;
 use Stef\SimpleCmsBundle\Entity\Page;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class ContactController extends Controller
+class ContactController extends BaseController
 {
-    public function contactAction()
+    public function contactAction(Request $request)
+    {
+        $form = $this->createForm(new ContactType());
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $contact = new Contact();
+                $contact->setEmail($form->get('email')->getData());
+                $contact->setName($form->get('name')->getData());
+                $contact->setReason($form->get('reason')->getData());
+                $contact->setPhone($form->get('phone')->getData());
+                $contact->setIp($request->getClientIp());
+                $contact->setSummary($form->get('summary')->getData());
+                $contact->setModified(new \DateTime());
+                $contact->setCreated(new \DateTime());
+
+                $manager = $this->getContactManager();
+                $manager->persistAndFlush($contact);
+
+                return $this->redirect($this->generateUrl('stef_gastouder_marjolein_page_contact'));
+            }
+        }
+
+        return $this->render('StefGastouderMarjoleinBundle:Default:contact.html.twig', [
+            'page' => $this->getContactPageData(),
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function getContactPageData()
     {
         $page = new Page();
         $page->setDescription('Voor meer informatie over de opvang bij Marjolein van Deventer kunt u altijd vrijblijvend contact opnemen. Ik stuur altijd een berichtje terug.');
@@ -15,20 +48,18 @@ class ContactController extends Controller
 
         $page->setBody('
             <h2>Direct contact via website</h2>
-            <p>Op dit moment is het nog niet mogelijk om direct via de website contact op te nemen. Dit gaat uiteraard spoedig veranderen zodat u meteen via de website al uw vragen bij mij kwijt kunt.</p>
+            <p>Via onderstaand formulier kan je een bericht sturen of vraag stellen. Uiteraard zal ik na ontvangst zo snel mogelijk een reactie sturen.</p>
             <h2>Via Facebook</h2>
-            <p>U kunt mij bereiken via de facebookpagina <a href="https://www.facebook.com/pages/Gastouder-Marjolein/1520167084916978">Gastouder Marjolein</a>. Houd er rekening mee dat als u een berichtje plaatst op die pagina, dit voor iedereen leesbaar is. Als u uw vraag liever in een priveberichtje bespreekt, geeft u dat dan aan en ik neem zo snel mogelijk contact met u op.</p>
+            <p>Ook ben ik te bereiken via de facebookpagina <a href="https://www.facebook.com/pages/Gastouder-Marjolein/1520167084916978">Gastouder Marjolein</a>.</p>
             <h2>Adres</h2>
-            <p>De opvang is nog niet geopend. Om u echter een idee te geven in welke omgeving u uw kind misschien wel langsbrengt geef ik hieronder toch alvast het adres.</p>
+            <p>De opvang is te vinden op onderstaand adres. Het bezoeken van de opvang bij voorkeur alleen op afspraak.</p>
             <p>
                 <strong>Marjolein van Deventer</strong></br>
-                Haringvlietstraat</br>
+                Haringvlietstraat 97</br>
                 3313 EB Dordrecht</br>
             </p>
         ');
 
-        return $this->render('StefGastouderMarjoleinBundle:Default:page.html.twig', [
-            'page' => $page
-        ]);
+        return $page;
     }
 }
